f5f47e3852b7e3557ed28d780b295db5f2aba245
neerajp99 <neerajp1999@gmail.com>
2019-09-11 06:06:11 +0530

    Initial route for user authentication

---
+++ routes/api/users.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+
+
+// @route POST /api/users/register
+// @description Reister New User
+// @access Public
+router.post('/register', (req, res) => {
+
+})
