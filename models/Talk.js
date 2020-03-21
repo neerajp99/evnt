0f0ef81a2a01b166e74cead9220cd9a35415d051
Kartik Goel <goel.kartik39@gmail.com>
2020-03-21 16:32:31 +0530

    Added Talk schema

--- models/Talk.js
+++ models/Talk.js
@@ -1,6 +1,31 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const talkSchema = new Schema({});
+const talkSchema = new Schema({
+    user : {
+        type : Schema.Types.ObjectId,
+        ref : "users"
+    },
+    title : {
+        type : String,
+        isRequired : true
+    },
+    elevatorPitch : {
+        type : String,
+        isRequired : true
+    },
+    talkDuration : {
+        type : Number,
+        isRequired : true
+    },
+    audienceLevel : {
+        type : String,
+        enum : ['All','Intermediate','Beginner','Expert']
+    },
+    description : {
+        type : String,
+    },
+
+});
 
 module.exports = Talk = mongoose.model("talks", talkSchema);
