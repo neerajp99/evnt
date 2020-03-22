d37baf0cd9a4677df42d477c949ccaffc0e46510
Kartik Goel <goel.kartik39@gmail.com>
2020-03-22 16:08:06 +0530

    Update Event Model

--- models/Event.js
+++ models/Event.js
@@ -2,6 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const EventSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "users"
+  },
   name: {
     type: String,
     required: true
