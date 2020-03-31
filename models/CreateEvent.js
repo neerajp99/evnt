fa28600dbd1ba08f9db12b2a0d3771836bcfd720
neerajp99 <neerajp1999@gmail.com>
2020-03-31 14:16:06 +0530

    Create new event create model

---
+++ models/CreateEvent.js
@@ -0,0 +1,64 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const createEventSchema = new Schema({
+  eventName: {
+    type: String,
+    required: true
+  },
+  eventDate: {
+    type: Date,
+    // for testing purpose
+    default: Date.now()
+  },
+  eventVenue: {
+    type: String,
+    required: true
+  },
+  eventDescription: {
+    type: String,
+    required: true
+  },
+  eventWebsite: {
+    type: String
+  },
+  recurringEvent: {
+    type: Boolean,
+    default: false
+  },
+  anonymousSubmission: {
+    type: Boolean,
+    default: true
+  },
+  cfpDescription: {
+    type: String,
+    required: true
+  },
+  cfpNotes: {
+    type: String
+  },
+  additionalDetails: {
+    type: String
+  },
+  talkDuration: [
+    {
+      talkFormat: {
+        type: String,
+        required: true
+      }
+    }
+  ],
+  talkTags: {
+    type: [String],
+    required: true
+  },
+  travelAssitance: {
+    type: Boolean,
+    default: false
+  },
+  travelAssitancePolicy: {
+    type: String
+  }
+});
+
+module.exports = CreateEvent = mongoose.model("createEvent", createEventSchema);
