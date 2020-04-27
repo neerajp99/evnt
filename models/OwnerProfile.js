add29339383389d9060981f5ece795e6aac13195
neerajp99 <neerajp1999@gmail.com>
2020-04-27 23:36:08 +0530

    Create schema for owner profiles

---
+++ models/OwnerProfile.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Owner's Profile Schema
+const ownerProfileSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "users"
+  },
+  handle: {
+    type: String,
+    isRequired: true
+  },
+  organisation: {
+    type: String
+  },
+  website: {
+    type: String
+  },
+  bio: {
+    type: String,
+    isRequired: true
+  },
+  social: {
+    twitter: {
+      type: String
+    },
+    facebook: {
+      type: String
+    },
+    github: {
+      type: String
+    },
+    linkedin: {
+      type: String
+    }
+  }
+});
+
+module.exports = OwnerProfile = mongoose.model("ownerProfile", ownerProfileSchema);
