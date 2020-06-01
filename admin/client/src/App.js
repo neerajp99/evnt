0af84aca1b1597a931d65d5f400a8b7c20a75266
neerajp99 <neerajp1999@gmail.com>
2020-06-01 17:19:47 +0530

    Update routing

--- admin/client/src/App.js
+++ admin/client/src/App.js
@@ -1,25 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.scss";
+import styled from "styled-components";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Login from "./components/authentication/Login"
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Route exact path = "/login" component = {Login}/>
     </div>
+    </Router>
   );
 }
 
