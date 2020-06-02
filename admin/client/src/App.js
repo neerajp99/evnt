b6d1fec7e41ac921ccab4c7fae3b8759c500059f
neerajp99 <neerajp1999@gmail.com>
2020-06-02 12:45:26 +0530

    Update App

--- admin/client/src/App.js
+++ admin/client/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import GlobalFonts from "./styles/fonts/fonts"
 import "./App.scss";
 import styled from "styled-components";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
