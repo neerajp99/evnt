0d39a15833d177996f9c5d37e972bd03e63c13ef
neerajp99 <neerajp1999@gmail.com>
2020-06-03 20:44:06 +0530

    Update app with redux store

--- admin/client/src/App.js
+++ admin/client/src/App.js
@@ -1,19 +1,24 @@
 import React from "react";
-import GlobalFonts from "./styles/fonts/fonts"
+import GlobalFonts from "./styles/fonts/fonts";
 import "./App.scss";
 import styled from "styled-components";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Login from "./components/authentication/Login"
-import Register from "./components/authentication/Register"
+import Login from "./components/authentication/Login";
+import Register from "./components/authentication/Register";
+// Bring in Provider for making store accessible to nested components
+import { Provider } from "react-redux";
+import store from "./store";
 
 function App() {
   return (
-    <Router>
-    <div className="App">
-        <Route exact path = "/login" component = {Login}/>
-        <Route exact path = "/register" component = {Register} />
-    </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
