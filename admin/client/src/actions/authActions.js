714df2f5bc2773f9ccb777c9f21658ec41090295
neerajp99 <neerajp1999@gmail.com>
2020-06-04 09:05:37 +0530

    Update actions for login and logout

--- admin/client/src/actions/authActions.js
+++ admin/client/src/actions/authActions.js
@@ -1,5 +1,7 @@
-import { GET_CURRENT_USER, GET_ERRORS } from "./types";
+import { SET_CURRENT_USER, GET_ERRORS } from "./types";
 import axios from "axios";
+import setAuthToken from "../utils/setAuthToken";
+import jwt_decode from "jwt-decode";
 
 // Resgiter user
 export const registerUser = (userData, history) => dispatch => {
@@ -14,4 +16,46 @@ export const registerUser = (userData, history) => dispatch => {
         payload: error.response.data
       });
     });
+};
+
+// Login User
+export const loginUser = (userData, history) => dispatch => {
+  axios
+    .post("/api/ownerUsers/login", userData)
+    .then(response => {
+      // Saving the token to local storage
+      const { token } = response.data;
+      localStorage.setItem("adminJWT", token);
+      // Set token to the authorization header
+      setAuthToken(token);
+      // Decrypt the access token to get user data
+      const decodedToken = jwt_decode(token);
+      // Dispatch the decoded token
+      dispatch(setCurrentAdmin(decodedToken));
+    })
+    .catch(error => {
+      dispatch({
+        type: GET_ERRORS,
+        payload: error.response.data
+      });
+    });
+};
+
+// Method to set logged in user
+export const setCurrentAdmin = decodedToken => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decodedToken
+  };
+};
+
+// Logout Admin
+export const logOutAdmin = () => dispatch => {
+  // Remove the tokens from the localstorage
+  localStorage.removeItem("adminJWT");
+  // Delete the authorization header
+  setAuthToken(false);
+  // Passing empty array to setCurrentAdmin
+  // Set isAuthenticated to false
+  dispatch(setCurrentAdmin({}));
 };
