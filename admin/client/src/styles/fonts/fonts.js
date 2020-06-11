d9ca6a694f192b9b648e9a59bf5b8147ee7f783d
neerajp99 <neerajp1999@gmail.com>
2020-06-11 17:32:28 +0530

    Add roboto font

--- admin/client/src/styles/fonts/fonts.js
+++ admin/client/src/styles/fonts/fonts.js
@@ -1,11 +1,18 @@
 import { createGlobalStyle, css } from "styled-components";
 
 import Lato from "../../styles/fonts/Lato.ttf";
+import Roboto from "../fonts/roboto/RobotoMono-Medium.ttf"
 
 export default createGlobalStyle`
   @font-face {
     font-family: Lato;
     src: url(${Lato}) format("truetype");
+    font-weight: 300;
+    font-style: normal;
+  }
+  @font-face {
+    font-family: Roboto;
+    src: url(${Roboto}) format("truetype");
     font-weight: 300;
     font-style: normal;
   }
