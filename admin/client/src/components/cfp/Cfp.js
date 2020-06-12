e64d2a29c3e44d25a2cda388482912f3894a9f6f
neerajp99 <neerajp1999@gmail.com>
2020-06-12 12:51:53 +0530

    Initial cfp

---
+++ admin/client/src/components/cfp/Cfp.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { Container, InnerContainer } from "../../styles/Commons";
+import Side from "../sidebar/Sidebar";
+
+class Cfp extends Component {
+  render() {
+    return (
+      <Container>
+        <Side />
+        <InnerContainer>Wowowow</InnerContainer>
+      </Container>
+    );
+  }
+}
+export default Cfp;
