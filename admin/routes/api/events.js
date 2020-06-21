9557bb12063c19225bab08d2031a3b52873f7ab6
Kartik Goel <goel.kartik39@gmail.com>
2020-06-21 17:34:35 +0530

    Fixed event routes

--- admin/routes/api/events.js
+++ admin/routes/api/events.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const Event = require("../../models/Event");
 const passport = require("passport");
 
-// @route GET /api/event
+// @route GET /api/events
 // @description Get event made by user
 // @access Private
 
@@ -39,23 +39,68 @@ router.post(
     const eventData = {};
     eventData.user = req.user.id;
 
-    if (req.body.name) {
-      eventData.name = req.body.name;
+    if (req.body.eventName) {
+      eventData.eventName = req.body.eventName;
     }
-    if (req.body.venue) {
-      eventData.venue = req.body.venue;
+    // Event begin date
+    if (req.body.eventBeginDate) {
+      eventData.eventBeginDate = req.body.eventBeginDate;
     }
-    if (req.body.organisation) {
-      eventData.startDate = req.body.startDate;
+    // Event End date
+    if (req.body.eventEndDate) {
+      eventData.eventEndDate = req.body.eventEndDate;
     }
-    if (req.body.endDate) {
-      eventData.endDate = req.body.endDate;
+    // Event Venue
+    if (req.body.eventVenue) {
+      eventData.eventVenue = req.body.eventVenue;
     }
-    if (req.body.description) {
-      eventData.description = req.body.description;
+    // Event Description - long string (might convert md5 hash etc)
+    if (req.body.eventDescription) {
+      eventData.eventDescription = req.body.eventDescription;
     }
-    if (req.body.website) {
-      eventData.website = req.body.website;
+    // Event Website
+    if (req.body.eventWebsite) {
+      eventData.eventWebsite = req.body.eventWebsite;
+    }
+    // Is event recurring?
+    if (req.body.recurringEvent) {
+      eventData.recurringEvent = req.body.recurringEvent;
+    }
+    // What's the event code of conduct*?
+    if (req.body.eventCodeOfConduct) {
+      eventData.eventCodeOfConduct = req.body.eventCodeOfConduct;
+    }
+    // Is event submission anonymous
+    if (req.body.anonymousSubmission) {
+      eventData.anonymousSubmission = req.body.anonymousSubmission;
+    }
+    // Event Call for Propsal Details
+    if (req.body.cfpDescription) {
+      eventData.cfpDescription = req.body.cfpDescription;
+    }
+    // Event CFP Notes
+    if (req.body.cfpNotes) {
+      eventData.cfpNotes = req.body.cfpNotes;
+    }
+    // Any additional details
+    if (req.body.additionalDetails) {
+      eventData.additionalDetails = req.body.additionalDetails;
+    }
+    // Talk durations
+    if (req.body.talkDuration) {
+      eventData.talkDuration = req.body.talkDuration;      
+    }
+    // Talk Tags
+    if (typeof req.body.talkTags !== "undefined") {
+      profileData.talkTags = req.body.talkTags.split(",");
+    }
+    // Is there travel assistance
+    if (req.body.travelAssistance) {
+      eventData.travelAssistance = req.body.travelAssistance;
+    }
+    // Travel Assistance policy, if any
+    if (req.body.travelAssistancePolicy) {
+      eventData.travelAssistancePolicy = req.body.travelAssistancePolicy;
     }
 
     // social links goes here
@@ -79,7 +124,7 @@ router.post(
         res.status(200).json(event);
       })
       .catch(err => {
-        res.status(404).json("Could not save the event data");
+        res.status(404).json(err);
       });
   }
 );
@@ -97,13 +142,24 @@ router.post(
         _id: req.params.id
       },
       {
-        name: req.body.name,
-        venue: req.body.venue,
-        startDate: req.body.startDate,
-        endDate: req.body.endDate,
-        description: req.body.description,
-        website: req.body.website,
-        outcome: req.body.outcome
+        eventName: req.body.eventName,
+        eventVenue: req.body.eventVenue,
+        eventStartDate: req.body.eventStartDate,
+        eventEndDate: req.body.eventEndDate,
+        eventDescription: req.body.eventDescription,
+        eventWebsite: req.body.eventWebsite,
+        recurringEvent : req.body.recurringEvent,
+        eventCodeOfConduct : req.body.eventCodeOfConduct,
+        anonymousSubmission : req.body.anonymousSubmission,
+        cfpDescription : req.body.cfpDescription,
+        cfpNotes : req.body.cfpNotes,
+        additionalDetails : req.body.additionalDetails,
+        talkDuration : req.body.talkDuration,
+        talkTags : req.body.talkTags,
+        travelAssistance : req.body.travelAssistance,
+        travelAssistancePolicy : requestAnimationFrame.body.travelAssistancePolicy,
+        social : req.body.social
+
         //    "social.facebook" : req.body.social.facebook,
         //    "social.twitter" : req.body.social.twitter,
         //    "youtube" : req.body.social.youtube,
