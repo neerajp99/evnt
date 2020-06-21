67f3870316ff0473a86c3eb4e181104264d09045
neerajp99 <neerajp1999@gmail.com>
2020-06-22 00:14:39 +0530

    Modified styles for cfp

--- admin/client/src/components/cfp/styles/Cfp.js
+++ admin/client/src/components/cfp/styles/Cfp.js
@@ -10,7 +10,7 @@ export const CfpContainer = styled.div`
   display: flex;
   flex-direction: column;
   overflow: hidden;
-  @media (max-width: 768px) {
+  @media (max-width: 1100px) {
     overflow-y: auto;
     /* margin-left: -0.3%; */
   }
@@ -31,9 +31,11 @@ export const CfpContainerBottom = styled.div`
   /* background: blue; */
   /* overflow: auto; */
   display: flex;
-  justify-content: space-around;
+  justify-content: space-between;
+  margin-left: -15px;
   @media (max-width: 1100px) {
     flex-direction: column;
+    margin-left: 0px;
   }
 `;
 
@@ -42,7 +44,7 @@ export const CfpContainerBottomContainer = styled.div`
   top: 4%;
   height: 93%;
   /* flex-grow: 1; */
-  width: 30%;
+  width: 29%;
   margin: 0 auto;
   border-radius: 10px;
   /* overflow-y: auto; */
@@ -59,7 +61,7 @@ export const CfpContainerBottomContainer = styled.div`
 export const CfpListContainer = styled.div`
   display: block;
   width: 100%;
-  height: 17%;
+  height: 21%;
   background: #fff;
   position: relative;
   margin: 0 auto;
@@ -67,7 +69,11 @@ export const CfpListContainer = styled.div`
   margin-bottom: 5%;
   /* top: 4%; */
   /* z-index: 5; */
-  /* background-color: ${props => (props.isDragging ? "red" : "white")} */
+  /* background-color: ${props => (props.isDragging ? "red" : "white")}; */
+  transition: background-color 0.3s ease;
+  cursor: grabbing !important;
+  cursor: -moz-grabbing;
+  cursor: -webkit-grabbing;
 `;
 
 export const CfpList = styled.div`
@@ -83,9 +89,13 @@ export const CfpList = styled.div`
   overflow: auto;
   background: #ebeff3;
   padding-top: 6%;
-  padding-left: 10px;
-  padding-right: 10px;
-  /* background: ${props => (props.isDraggingOver ? "blue" : "#ebeff3")} */
+  padding-left: 15px;
+  padding-right: 15px;
+  background-color: ${props => (props.isDraggingOver ? "#e6e9ed" : "#ebeff3")};
+  transition: background-color 0.3s ease;
+  @media (max-width: 1100px) {
+    padding: 0px;
+  }
 `;
 
 export const CfpHeader = styled.div`
@@ -96,4 +106,171 @@ export const CfpHeader = styled.div`
   font-family: Lato;
   font-weight: 700;
   color: #9fa4a9;
+`;
+
+export const TalkStatus = styled.div`
+  background: ${props => (props.isOpen ? "#bdccf3" : "#fae4e3")};
+  position: absolute;
+  left: 4%;
+  top: 7%;
+  font-family: Lato;
+  text-transform: uppercase;
+  font-size: 12.5px;
+  padding: 3px 6px 2px;
+  color: ${props => (props.isOpen ? "#7690d9" : "#d0696f")};
+  font-weight: 700;
+  letter-spacing: 0.1rem;
+`;
+
+export const TalkTitle = styled.div`
+  position: absolute;
+  top: 32%;
+  font-family: Lato;
+  left: 4%;
+  color: #616e76;
+  text-align: left;
+`;
+
+export const SpeakerContent = styled.div`
+  position: absolute;
+  top: 70%;
+  left: 4%;
+  display: inline-block;
+`;
+
+export const SpeakerTag = styled.div`
+  display: inline-block;
+  background: #e5f1f4;
+  height: 30px;
+  width: 30px;
+  border-radius: 100%;
+  font-family: Lato;
+  font-size: 15px;
+  line-height: 30px;
+  color: #35c6d2;
+`;
+
+export const SpeakerName = styled.div`
+  display: inline-block;
+  position: relative;
+  left: 12%;
+  color: #8198a5;
+  font-size: 15px;
+  font-family: Lato;
+`;
+
+export const TalkModal = styled.div`
+  position: absolute;
+  display: inline-block;
+  height: 20px;
+  width: 20px;
+  right: 1%;
+  top: 73%;
+  cursor: pointer;
+  span {
+    margin-bottom: 0.2rem;
+    background: #a9b2bd;
+    height: 3px;
+    width: 15%;
+    display: block;
+    border-radius: 100%;
+  }
+`;
+
+export const TalkDetails = styled.div`
+  width: 85%;
+  min-height: 50%;
+  height: auto;
+  /* background: red; */
+  position: relative;
+  top: 10%;
+  left: 4%;
+`;
+export const TalkHeading = styled.div`
+  color: #56585f;
+  font-family: Lato;
+  font-size: 30px;
+  font-weight: 700;
+  text-align: left;
+`;
+
+export const TalkDescription = styled.div`
+  position: relative;
+  color: #81869a;
+  margin-top: 3%;
+  font-family: Lato;
+  text-align: left;
+`;
+
+export const TalkTags = styled.div`
+  margin-top: 4%;
+  display: inline-block;
+  width: 95%;
+  margin-left: -3%;
+`;
+
+export const TalkTag = styled.div`
+  display: inline-block;
+  margin-left: 3%;
+  background-color: ${props => (props.index % 2 == 0 ? "#e9f2fe" : "#fff6ec")};
+  color: ${props => (props.index % 2 === 0 ? "#749ad4" : "#c9977c")};
+  padding: 6px 17px 6px;
+  margin-top: 1.5%;
+  font-size: 14px;
+`;
+
+export const TalkActivity = styled.div`
+  min-height: 30%;
+  width: 96%;
+  /* background: tomato; */
+  position: relative;
+  top: 15%;
+  left: 4%;
+  margin-bottom: 10%;
+`;
+
+export const TalkActivityTopContainer = styled.div`
+  position: relative;
+  height: 50px;
+  width: 40%;
+  display: inline-block;
+`;
+
+export const TalkActivityIcon = styled.div`
+  display: inline-block;
+  color: #55575f;
+`;
+
+export const TalkActivityHeading = styled.div`
+  display: inline-block;
+  position: relative;
+  left: 3%;
+  font-size: 17px;
+  font-weight: 600;
+  color: #66686f;
+  top: 3%;
+`;
+
+export const TalkActivityFlex = styled.div`
+  display: flex;
+  /* grid-template-columns: repeat(auto-fit, minmax(19rem, 1fr)); */
+  min-height: 60vh;
+  height: auto;
+  width: 100%;
+  flex-direction: row;
+  @media(max-width: 1100px) {
+      flex-direction: column;
+  }
+`;
+
+export const TalkComments = styled.div`
+  /* background: blue; */
+  flex: 0 1 auto;
+  width: 60%;
+`;
+
+export const TalkOtherDetails = styled.div`
+  background: #e6e9ed;
+  flex: 1 1 auto;
+  width: 40%;
 `;
