7cb08c5becd935f529df36b8b2be209050b16906
neerajp99 <neerajp1999@gmail.com>
2020-06-22 00:14:28 +0530

    modified dummy data

--- admin/client/src/components/cfp/dummy_data.js
+++ admin/client/src/components/cfp/dummy_data.js
@@ -2,24 +2,51 @@ const initalData = {
   talks: {
     "talk-1": {
       id: "talk-1",
-      content: "Take out the garbage"
+      content: "The Joy of creaying art with code",
+      speaker: "Andrej Muller",
+      status: true,
+      description:
+        "Vivamus integer non suscipit taciti mus etiam at primis tempor sagittis sit, euismod libero facilisi aptent elementum felis blandit cursus gravida sociis erat ante, eleifend lectus nullam dapibus netus feugiat curae curabitur est ad. Massa curae fringilla porttitor quam sollicitudin iaculis aptent leo ligula euismod dictumst, orci penatibus mauris eros etiam praesent erat volutpat posuere hac. Metus fringilla nec ullamcorper odio aliquam lacinia conubia mauris tempor, etiam ultricies proin quisque lectus sociis id tristique, integer phasellus taciti pretium adipiscing tortor sagittis ligula.",
+      tags: ["visualizations", "python", "physics"]
     },
     "talk-2": {
       id: "talk-2",
-      content: "Take out the bitch"
+      content: "An Introduction towith Python Web Scraping ",
+      speaker: "James Norwell",
+      status: false,
+      description:
+        "Vivamus integer non suscipit taciti mus etiam at primis tempor sagittis sit, euismod libero facilisi aptent elementum felis blandit cursus gravida sociis erat ante, eleifend lectus nullam dapibus netus feugiat curae curabitur est ad. Massa curae fringilla porttitor quam sollicitudin iaculis aptent leo ligula euismod dictumst, orci penatibus mauris eros etiam praesent erat volutpat posuere hac. Metus fringilla nec ullamcorper odio aliquam lacinia conubia mauris tempor, etiam ultricies proin quisque lectus sociis id tristique, integer phasellus taciti pretium adipiscing tortor sagittis ligula.",
+      tags: ["scipy", "pydata", "django"]
     },
     "talk-3": {
       id: "talk-3",
-      content: "Take out the carpet"
+      content: "Django Testing on Steroid: pytest + Hypothesis",
+      speaker: "Hiraj Yayal",
+      status: true,
+      description:
+        "Vivamus integer non suscipit taciti mus etiam at primis tempor sagittis sit, euismod libero facilisi aptent elementum felis blandit cursus gravida sociis erat ante, eleifend lectus nullam dapibus netus feugiat curae curabitur est ad. Massa curae fringilla porttitor quam sollicitudin iaculis aptent leo ligula euismod dictumst, orci penatibus mauris eros etiam praesent erat volutpat posuere hac. Metus fringilla nec ullamcorper odio aliquam lacinia conubia mauris tempor, etiam ultricies proin quisque lectus sociis id tristique, integer phasellus taciti pretium adipiscing tortor sagittis ligula.",
+      tags: ["flask", "django", "apache"]
     },
     "talk-4": {
       id: "talk-4",
-      content: "kakak garbage"
-  },
-  "talk-5": {
-    id: "talk-5",
-    content: "kakak jajaajajaj"
-  }
+      content:
+        "Lessons from the Trenches: rewriting and re-releasing virtualenv",
+      speaker: "Biral Bhah",
+      status: true,
+      description:
+        "Vivamus integer non suscipit taciti mus etiam at primis tempor sagittis sit, euismod libero facilisi aptent elementum felis blandit cursus gravida sociis erat ante, eleifend lectus nullam dapibus netus feugiat curae curabitur est ad. Massa curae fringilla porttitor quam sollicitudin iaculis aptent leo ligula euismod dictumst, orci penatibus mauris eros etiam praesent erat volutpat posuere hac. Metus fringilla nec ullamcorper odio aliquam lacinia conubia mauris tempor, etiam ultricies proin quisque lectus sociis id tristique, integer phasellus taciti pretium adipiscing tortor sagittis ligula.",
+      tags: ["nlp", "visualizations", "beginner", "data-processing", "numpy", "nlp", "visualizations", "beginner", "data-processing", "numpy", "nlp", "visualizations", "beginner", "data-processing", "numpy"]
+    },
+    "talk-5": {
+      id: "talk-5",
+      content:
+        "Diffprivlib: Privacy-preserving machine learning with Scikit-learn",
+      speaker: "Jason Andre",
+      status: true,
+      description:
+        "Vivamus integer non suscipit taciti mus etiam at primis tempor sagittis sit, euismod libero facilisi aptent elementum felis blandit cursus gravida sociis erat ante, eleifend lectus nullam dapibus netus feugiat curae curabitur est ad. Massa curae fringilla porttitor quam sollicitudin iaculis aptent leo ligula euismod dictumst, orci penatibus mauris eros etiam praesent erat volutpat posuere hac. Metus fringilla nec ullamcorper odio aliquam lacinia conubia mauris tempor, etiam ultricies proin quisque lectus sociis id tristique, integer phasellus taciti pretium adipiscing tortor sagittis ligula.",
+      tags: ["visualizations", "python", "physics", "http"]
+    }
   },
   columns: {
     "column-1": {
@@ -35,8 +62,8 @@ const initalData = {
     "column-3": {
       id: "column-3",
       title: "Finalised",
-      talksId: []
-    },
+      talksId: ["talk-5"]
+    }
   },
   columnOrder: ["column-1", "column-2", "column-3"]
 };
