6275de35be7eaa45f84542e92b5bbc25825934b3
neerajp99 <neerajp1999@gmail.com>
2020-06-23 07:12:51 +0530

    Modified props

--- admin/client/src/components/cfp/CommentInput.js
+++ admin/client/src/components/cfp/CommentInput.js
@@ -1,17 +1,29 @@
-import React from 'react'
-import {
-    CommentInputField
-} from "./styles/Cfp"
+import React from "react";
+import PropTypes from "prop-types";
+import { CommentInputField } from "./styles/Cfp";
 
-function CommentInput(props){
-    return (
-        <CommentInputField
-            placeholder = {props.placeholder}
-            id = {props.id}
-        >
-
-        </CommentInputField>
-    )
+function CommentInput(props) {
+  return (
+    <CommentInputField
+      placeholder={props.placeholder}
+      id={props.id}
+      name={props.name}
+      value={props.value}
+      type={props.type}
+      onChange={props.onChange}
+    />
+  );
 }
 
-export default CommentInput
+CommentInput.propTypes = {
+  placeholder: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  type: PropTypes.string.isRequired
+};
+CommentInput.defaultProps = {
+  type: "text"
+};
+
+export default CommentInput;
