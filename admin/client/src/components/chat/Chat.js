1cc72170d5c8c84761bffe7efd1914c340af6b61
neerajp99 <neerajp1999@gmail.com>
2020-06-25 07:10:28 +0530

    Initial chat component

---
+++ admin/client/src/components/chat/Chat.js
@@ -0,0 +1,34 @@
+import React, { Component, useEffect } from "react";
+import Side from "../sidebar/Sidebar";
+import { Container, InnerContainer } from "../../styles/Commons";
+import socketIOClient from "socket.io-client";
+const ENDPOINT = "http://127.0.0.1:4004";
+
+let socket;
+
+class Chat extends Component {
+  state = {
+    message: ""
+  };
+  componentDidMount() {
+    let socket = socketIOClient(ENDPOINT);
+
+    console.log(socket);
+    return () => {
+      socket.emit("disconnect");
+
+      socket.off();
+    };
+  }
+
+  render() {
+    return (
+      <Container>
+        <Side />
+        <InnerContainer />
+      </Container>
+    );
+  }
+}
+
+export default Chat;
