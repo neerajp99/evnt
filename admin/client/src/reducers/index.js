3f0b3d74c6e86b3fbcdac00b74713081e856eca9
neerajp99 <neerajp1999@gmail.com>
2020-07-03 10:22:09 +0530

    Update root reducer

--- admin/client/src/reducers/index.js
+++ admin/client/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import authReducer from "./authReducer";
+import profileReducer from "./profileRedcuer"
 
 export default combineReducers({
-  auth: authReducer
+  auth: authReducer,
+  profile: profileReducer
 });
