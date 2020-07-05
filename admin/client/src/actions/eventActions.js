d343800e047a8f65b86d08a65fb20890feb6e6aa
neerajp99 <neerajp1999@gmail.com>
2020-07-05 22:33:33 +0530

    Add get event action

--- admin/client/src/actions/eventActions.js
+++ admin/client/src/actions/eventActions.js
@@ -1,4 +1,9 @@
-import { CREATE_EVENT, GET_EVENT_DETAILS, GET_ERRORS } from "./types";
+import {
+  CREATE_EVENT,
+  GET_EVENT_DETAILS,
+  GET_ERRORS,
+  LOAD_EVENT
+} from "./types";
 import axios from "axios";
 
 // Action to create an event
@@ -6,6 +11,7 @@ export const createEvent = (data, history) => dispatch => {
   axios
     .post("/api/events", data)
     .then(response => {
+      console.log(response);
       history.push("/dashboard");
     })
     .catch(error => {
@@ -14,4 +20,30 @@ export const createEvent = (data, history) => dispatch => {
         payload: error.response.data
       });
     });
+};
+
+// Action to fetch event details
+export const getEvent = () => dispatch => {
+  dispatch(setEventLoading());
+  axios
+    .get("/api/events")
+    .then(response => {
+      dispatch({
+        type: GET_EVENT_DETAILS,
+        payload: response.data
+      });
+    })
+    .catch(error => {
+      dispatch({
+        type: GET_EVENT_DETAILS,
+        payload: {}
+      });
+    });
+};
+
+// Profile Loading
+export const setEventLoading = () => {
+  return {
+    type: LOAD_EVENT
+  };
 };
