16e586e573265f56650098c113ad9536634e888a
neerajp99 <neerajp1999@gmail.com>
2020-07-27 05:03:25 +0530

    Add talk routes

--- speaker/client/src/App.js
+++ speaker/client/src/App.js
@@ -1,22 +1,23 @@
-import React from "react"
+import React from "react";
 import "./App.scss";
 import styled from "styled-components";
-import {BrowserRouter as Router,Route,Switch} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from "./components/authentication/Login";
 import Register from "./components/authentication/Register";
 import Dashboard from "./components/dashboard/Dashboard";
+import Talk from "./components/talk/Talk";
 //Making the global store available to all the components
-import {Provider} from "react-redux";
+import { Provider } from "react-redux";
 import store from "./store";
 import setAuthToken from "./util/setAuthToken";
 import jwt_decode from "jwt-decode";
-import {setCurrentSpeaker,logoutSpeaker} from './actions/authActions';
+import { setCurrentSpeaker, logoutSpeaker } from "./actions/authActions";
 import ProtectedRoute from "./components/commons/ProtectedRoute";
 import Swal from "sweetalert2";
 
 //Checking if user is logged in
 const token = localStorage.getItem("token");
-if(token) {
+if (token) {
   //Set token in authorization Header for all requests
   setAuthToken(token);
 
@@ -27,18 +28,17 @@ if(token) {
   store.dispatch(setCurrentSpeaker(decodedToken));
 
   //Checking for the expired token
-  const currentTime = new Date()/1000;
-  if(decodedToken.exp < currentTime) {
-
+  const currentTime = new Date() / 1000;
+  if (decodedToken.exp < currentTime) {
     //Logout speaker
     store.dispatch(logoutSpeaker({}));
 
     //Creating an alert for speaker user to know
     Swal.fire({
-      title : "Session Expired",
-      text : 'Please login again',
-      icon : "info",
-      confirmButtonText : 'Got it'
+      title: "Session Expired",
+      text: "Please login again",
+      icon: "info",
+      confirmButtonText: "Got it"
     });
     window.location.href = "/login";
   }
@@ -46,24 +46,22 @@ if(token) {
 
 const App = () => {
   return (
-    <Provider store = {store}>
+    <Provider store={store}>
       <Router>
-        <div className = "App">
-          <Route exact path = "/login" component = {Login} />
-          <Route exact path = "/register" component = {Register} />
+        <div className="App">
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
         </div>
 
         <Switch>
-          <ProtectedRoute
-            exact
-            path = "/dashboard"
-            component = {Dashboard}
-          />
+          <ProtectedRoute exact path="/dashboard" component={Dashboard} />
         </Switch>
-
+        <Switch>
+          <ProtectedRoute exact path="/talk" component={Talk} />
+        </Switch>
       </Router>
     </Provider>
-  )
-}
+  );
+};
 
-export default App
+export default App;
