9a8d93c914908d5bbc9cafec9fd2d6db90b5270d
neerajp99 <neerajp1999@gmail.com>
2020-07-27 05:05:35 +0530

    Update talk model

--- speaker/models/Talk.js
+++ speaker/models/Talk.js
@@ -2,40 +2,39 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const talkSchema = new Schema({
-    user : {
-        type : Schema.Types.ObjectId,
-        ref : "speakerAuth"
-    },
-    title : {
-        type : String,
-        isRequired : true
-    },
-    elevatorPitch : {
-        type : String,
-        isRequired : true
-    },
-    talkDuration : {
-        type : Number,
-        isRequired : true
-    },
-    audienceLevel : {
-        type : String,
-        enum : ['All','Intermediate','Beginner','Expert']
-    },
-    description : {
-        type : String,
-    },
-    additionalDetails : {
-        type : String
-    },
-    outcome : {
-        type : String
-    },
-    shortlisted : {
-        type : String,
-        default : "false"
-    }
-
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "speakerAuth"
+  },
+  title: {
+    type: String,
+    isRequired: true
+  },
+  elevatorPitch: {
+    type: String,
+    isRequired: true
+  },
+  talkDuration: {
+    type: String,
+    isRequired: true
+  },
+  audienceLevel: {
+    type: String,
+    enum: ["All", "Intermediate", "Beginner", "Expert"]
+  },
+  description: {
+    type: String
+  },
+  additionalDetails: {
+    type: String
+  },
+  outcome: {
+    type: String
+  },
+  shortlisted: {
+    type: String,
+    default: "false"
+  }
 });
 
 module.exports = Talk = mongoose.model("talks", talkSchema);
