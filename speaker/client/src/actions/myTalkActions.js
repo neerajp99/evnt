47487643e775778d1eed4df9c19a221aadde7014
neerajp99 <neerajp1999@gmail.com>
2020-08-02 00:20:39 +0530

    Add my talk actions

---
+++ speaker/client/src/actions/myTalkActions.js
@@ -0,0 +1,28 @@
+import { GET_MY_TALKS, GET_ERRORS, LOADING_TALKS } from "./types";
+import axios from "axios";
+
+export const getMyTalks = () => dispatch => {
+  dispatch(setTalksLoading());
+  axios
+    .get("/api/talk")
+    .then(response => {
+        console.log(response)
+      dispatch({
+        type: GET_MY_TALKS,
+        payload: response.data.talk
+      });
+    })
+    .catch(error => {
+      dispatch({
+        type: GET_MY_TALKS,
+        payload: {}
+      });
+    });
+};
+
+// My talks loading
+export const setTalksLoading = () => {
+  return {
+    type: LOADING_TALKS
+  };
+};
