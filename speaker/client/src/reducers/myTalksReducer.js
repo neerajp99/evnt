90458dd03c3d85315d7ec5647b4e80dfb5dbc752
neerajp99 <neerajp1999@gmail.com>
2020-08-02 00:19:52 +0530

    Add reducer for mytalks

---
+++ speaker/client/src/reducers/myTalksReducer.js
@@ -0,0 +1,25 @@
+import { GET_MY_TALKS, LOADING_TALKS } from "../actions/types";
+
+// Initialising the state object for my submitted talks
+const initialState = {
+  myTalks: null,
+  myTalksloading: false
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case GET_MY_TALKS:
+      return {
+        ...state,
+        myTalks: action.payload,
+        myTalksloading: false
+      };
+    case LOADING_TALKS:
+      return {
+        ...state,
+        myTalksloading: true
+      };
+    default:
+      return state;
+  }
+}
