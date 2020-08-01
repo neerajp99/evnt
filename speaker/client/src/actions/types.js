d4f7820c3ecd094e1f07216fdf7ff6a3f915a501
neerajp99 <neerajp1999@gmail.com>
2020-08-02 00:21:35 +0530

    Update root action dispatcher

--- speaker/client/src/actions/types.js
+++ speaker/client/src/actions/types.js
@@ -1,3 +1,8 @@
 export const GET_ERRORS = "GET_ERRORS";
 export const SET_CURRENT_USER = "SET_CURRENT_USER";
 export const CREATE_TALK = "CREATE_TALK"
+export const SET_USER_PROFILE = "SET_PROFILE"
+export const GET_CURRENT_PROFILE = "GET_CURRENT_PROFILE"
+export const LOAD_PROFILE = "LOAD_PROFILE"
+export const GET_MY_TALKS = "GET_MY_TALKS"
+export const LOADING_TALKS = "LOADING_TALKS"
