f3e26a6906a4cc1e0b324405f8368d5821642d54
neerajp99 <neerajp1999@gmail.com>
2020-08-20 16:10:25 +0530

    Update talks actions

--- speaker/client/src/actions/talkActions.js
+++ speaker/client/src/actions/talkActions.js
@@ -1,15 +1,17 @@
-import {CREATE_TALK, GET_ERRORS} from './types.js'
-import axios from 'axios'
+import { CREATE_TALK, GET_ERRORS, LOADING_TALKS } from "./types.js";
+import axios from "axios";
 
-// Creating a talk 
+// Creating a talk
 export const createTalk = (talkData, history) => dispatch => {
-	console.log('yahaa')
-	axios.post("/api/talk", talkData).then(response => {
-		history.push('/dashboard')
-	}).catch(error => {
-		dispatch({
-			type: GET_ERRORS,
-			payload: error.response.data
-		})
-	})
-}
+  axios
+    .post("/api/talk", talkData)
+    .then(response => {
+      history.push("/dashboard");
+    })
+    .catch(error => {
+      dispatch({
+        type: GET_ERRORS,
+        payload: error.response.data
+      });
+    });
+};
