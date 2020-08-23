77cf0faaf00b0ef180b5846e477ae6cdec2f59bc
neerajp99 <neerajp1999@gmail.com>
2020-08-23 20:22:37 +0530

    Update root reducer

--- speaker/client/src/reducers/index.js
+++ speaker/client/src/reducers/index.js
@@ -2,9 +2,11 @@ import {combineReducers} from "redux"
 import authReducer from "./authReducer";
 import profileReducer from "./profileReducer";
 import myTalksReducer from "./myTalksReducer"
+import cfpReducer from "./cfpReducer.js"
 
 export default combineReducers({
     auth : authReducer,
     profile: profileReducer,
-    mytalks: myTalksReducer
+    mytalks: myTalksReducer,
+    cfpDetails: cfpReducer
 })
