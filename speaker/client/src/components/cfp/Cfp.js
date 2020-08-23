b8d71fcb463c961e92b2850a7db81520a63be7da
neerajp99 <neerajp1999@gmail.com>
2020-08-23 20:23:15 +0530

    Add lifecycle hooks

--- speaker/client/src/components/cfp/Cfp.js
+++ speaker/client/src/components/cfp/Cfp.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, withRouter } from "react-router-dom";
 import Side from "../sidebar/Sidebar";
 import { Container, InnerContainer } from "../../styles/Commons";
 import { makeStyles } from "@material-ui/core/styles";
@@ -8,15 +8,24 @@ import AccordionSummary from "@material-ui/core/AccordionSummary";
 import AccordionDetails from "@material-ui/core/AccordionDetails";
 import Typography from "@material-ui/core/Typography";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import { connect } from "react-redux";
+import { getCfpDetails } from "../../actions/cfpActions.js"
+import PropTypes from "prop-types";
+
 import {
-  CfpTop,
+  CfpTop, 
   CfpBottom,
   CfpDetailsLeft,
   CfpDetailsRight,
   CfpDescription,
   DottedCircle,
   EventTitle,
-  EventDescription
+  EventDescription,
+  CfpOtherDetails,
+  CfpList,
+  CfpListItem,
+  CfpItemSpan,
+  CfpButton
 } from "./styles/Cfp";
 
 const useStyles = makeStyles(theme => ({
@@ -31,6 +40,83 @@ const useStyles = makeStyles(theme => ({
 
 function Cfp(props) {
   const classes = useStyles();
+
+  const [cfpDetails, setCfpDetails] = useState(null)
+  const [eventBeginDate, setEventBeginDate] = useState("")
+  const [eventEndDate, setEventEndDate] = useState("")
+  const [totalSpeakers, setTotalSpeaker] = useState(30)
+  const [eventVenue, setEventVenue] = useState("")
+  const [eventCodeOfConduct, setEventCodeOfConduct] = useState("")
+  const [cfpDescription, setCfpDescription] = useState("")
+  const [cfpNotes, setCfpNotes] = useState("")
+  const [additionalDetails, setAdditionalDetails] = useState("")
+  const [travelAssistancePolicy, setTravelAssistancePolicy] = useState("")
+  const [eventWebsite, setEventWebsite] = useState("")
+
+  useEffect(() => {
+    if (props.cfpDetails.cfpDetails !== null) {
+      if (props.cfpDetails.cfpDetails) {
+        const {cfpDetails} = props.cfpDetails
+        console.log(cfpDetails[0])
+        setCfpDetails(cfpDetails[0])
+        setCfpDescription(cfpDetails[0].cfpDescription)
+        setEventBeginDate(cfpDetails[0].eventBeginDate.slice(0, 10))
+        setEventEndDate(cfpDetails[0].eventEndDate.slice(0, 10))
+        setEventVenue(cfpDetails[0].eventVenue)
+        setEventCodeOfConduct(cfpDetails[0].eventCodeOfConduct)
+        setCfpNotes(cfpDetails[0].cfpNotes)
+        setAdditionalDetails(cfpDetails[0].additionalDetails)
+        setTravelAssistancePolicy(cfpDetails[0].travelAssistancePolicy)
+        setEventWebsite(cfpDetails[0].eventWebsite)
+      }
+    }
+  }, [props.cfpDetails])
+
+  const details = [
+    "Event Code of Conduct",
+    "Call for proposal description.",
+    "Notes for Call for Proposals.",
+    "Additional Details",
+    "Travel Assistance Policy, if any."
+  ]
+
+  const detailsId = [
+    eventCodeOfConduct,
+    cfpDescription,
+    cfpNotes,
+    additionalDetails,
+    travelAssistancePolicy
+  ]
+
+
+  const {getCfpDetails} = props
+  useEffect(() => {
+    getCfpDetails()
+  }, [getCfpDetails])
+
+
+  
+
+  const AccordionContent = details.map((item, key) => {
+    return (
+        <Accordion key={key}>
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel1a-content"
+                  id="panel1a-header"
+                >
+                  <Typography className={classes.heading}>
+
+                  {item}
+                  </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography>{detailsId[key]}</Typography>
+                </AccordionDetails>
+              </Accordion>
+      )
+  })
+
   return (
     <Container>
       <Side />
@@ -41,63 +127,51 @@ function Cfp(props) {
             <EventTitle> PyCon France 2021</EventTitle>
             <EventDescription>
               Lorem ipsum dolor sit amet consectetur adipiscing elit, urna
+              Lorem ipsum dolor sit amet consectetur adipiscing elit, urna
               consequat felis vehicula class ultricies mollis dictumst, aenean
               non a in donec nulla. Phasellus ante pellentesque erat cum risus
               consequat imperdiet aliquam, integer placerat et turpis mi eros
               nec lobortis taciti, vehicula nisl litora tellus ligula porttitor
-              metus.{" "}
+              metus. Phasellus ante pellentesque erat cum risus
+              consequat imperdiet aliquam, integer placerat et turpis mi eros
+              nec lobortis taciti, vehicula nisl litora tellus ligula porttitor
+              consequat felis vehicula class ultricies mollis dictumst, aenean
+              non a in donec nulla. Phasellus ante pellentesque erat cum risus
+              consequat imperdiet aliquam, integer placerat et turpis mi eros
+              nec lobortis taciti, vehicula nisl litora tellus ligula porttitor
+              metus. Phasellus ante pellentesque erat cum risus
+              consequat imperdiet aliquam, integer placerat et turpis mi eros
+              nec lobortis taciti, vehicula nisl litora tellus ligula porttitor{" "}
             </EventDescription>
           </CfpTop>
           <CfpBottom>
             <CfpDetailsLeft>
-              <Accordion>
-                <AccordionSummary
-                  expandIcon={<ExpandMoreIcon />}
-                  aria-controls="panel1a-content"
-                  id="panel1a-header"
-                >
-                  <Typography className={classes.heading}>
-                    alalalalalalalaa
-                  </Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                  <Typography>i am neeraj</Typography>
-                </AccordionDetails>
-              </Accordion>
-
-              <Accordion>
-                <AccordionSummary
-                  expandIcon={<ExpandMoreIcon />}
-                  aria-controls="panel1a-content"
-                  id="panel1a-header"
-                >
-                  <Typography className={classes.heading}>
-                    alalalalalalalaa
-                  </Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                  <Typography>i am neeraj</Typography>
-                </AccordionDetails>
-              </Accordion>
+              {AccordionContent}
 
 
-              <Accordion>
-                <AccordionSummary
-                  expandIcon={<ExpandMoreIcon />}
-                  aria-controls="panel1a-content"
-                  id="panel1a-header"
-                >
-                  <Typography className={classes.heading}>
-                    alalalalalalalaa
-                  </Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                  <Typography>i am neeraj</Typography>
-                </AccordionDetails>
-              </Accordion>
+
             </CfpDetailsLeft>
             <CfpDetailsRight>
-            
+            <CfpOtherDetails>
+                <CfpList>
+                    <CfpListItem style = {{'fontSize': 50}}>30+</CfpListItem>
+                    <CfpItemSpan>Speakers</CfpItemSpan>
+
+                    <CfpListItem>2{eventBeginDate}</CfpListItem>
+                    <CfpItemSpan>Event Starts</CfpItemSpan>
+
+                    <CfpListItem>{eventEndDate}</CfpListItem>
+                    <CfpItemSpan>Event ends</CfpItemSpan>
+
+                    <CfpListItem style={{'color': '#4ca1ff'}}>{eventWebsite}</CfpListItem>
+                    <CfpItemSpan>Event Website</CfpItemSpan>
+
+                    <CfpListItem>{eventVenue}</CfpListItem>
+                    <CfpItemSpan>Event Venue</CfpItemSpan>
+
+                    <CfpButton>Event →</CfpButton>
+                </CfpList>
+            </CfpOtherDetails>
             </CfpDetailsRight>
           </CfpBottom>
         </CfpDescription>
@@ -106,4 +180,12 @@ function Cfp(props) {
   );
 }
 
-export default Cfp;
+// Cfp.propTypes = {
+//   getCfpDetails: PropTypes.func.isRequired,
+// }
+
+const mapStateToProps = state => ({
+  cfpDetails: state.cfpDetails
+})
+
+export default connect(mapStateToProps, {getCfpDetails})(Cfp)
