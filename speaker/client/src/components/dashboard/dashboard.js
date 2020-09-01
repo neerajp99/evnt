67fd780e055abcd6196e1726730d5dbefde1a790
neerajp99 <neerajp1999@gmail.com>
2020-09-01 21:54:36 +0530

    Modified dashboard component

--- speaker/client/src/components/dashboard/dashboard.js
+++ speaker/client/src/components/dashboard/dashboard.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
+import { connect } from "react-redux";
 import Side from "../sidebar/Sidebar";
 import {
   DashboardTop,
@@ -14,14 +15,18 @@ import {
   DashboardAvatar,
   DashboardGreetings,
   DashboardButtons,
-  DashboardButton,
+  DashboardButton
 } from "./styles/Dashboard";
 import { Container, InnerContainer } from "../../styles/Commons";
 import icon1 from "../../util/img/icon1.svg";
 import icon2 from "../../util/img/icon2.svg";
 import icon3 from "../../util/img/icon3.svg";
+import { getDashboard } from "../../actions/dashboardActions.js";
 
-class Dashboard extends Component { 
+class Dashboard extends Component {
+  componentDidMount() {
+    this.props.getDashboard();
+  }
   render() {
     return (
       <Container>
@@ -29,72 +34,79 @@ class Dashboard extends Component {
         <InnerContainer>
           <DashboardContainer>
             <DashboardTop>
-
-            <DashboardAvatar></DashboardAvatar>
-            <DashboardGreetings>
-            <span style={{"fontWeight": "600"}}>Welcome</span> <span>Neeraj!</span>
-            <br />
-            <span style={{"fontSize": "1.3rem"}}>Your Personalised Speaker Dashboard.</span>
-            </DashboardGreetings>
-            <DashboardButtons>
-            <DashboardButton>
-            Event Website
-            </DashboardButton>
-
-
-            </DashboardButtons>
-
+              <DashboardAvatar />
+              <DashboardGreetings>
+                <span style={{ fontWeight: "600" }}>Welcome</span>{" "}
+                <span>Neeraj!</span>
+                <br />
+                <p style={{ fontSize: "1.3rem" }}>
+                  Your Personalised Speaker Dashboard.
+                </p>
+              </DashboardGreetings>
+              <DashboardButtons>
+                <DashboardButton>Event Website</DashboardButton>
+              </DashboardButtons>
             </DashboardTop>
 
             <DashboardBottom>
-              <BottomFlex>
-                <DashboardBottomBox>
-                  <Icon>
-                    <img
-                      style={{ height: "40px", width: "40px" }}
-                      src={icon1}
-                      alt=""
-                    />
-                  </Icon>
-                  <IconText><span style={{'color': '#fff'}}>Complete</span> profile. Your Profile section includes a form to add/update profile with personal details, social links and avatar.</IconText>
-                </DashboardBottomBox>
+              <DashboardBottomBox>
+                <Icon>
+                  <img
+                    style={{ height: "40px", width: "40px" }}
+                    src={icon1}
+                    alt=""
+                  />
+                </Icon>
+                <IconText>
+                  <span style={{ color: "#fff" }}>Complete</span> profile. Your
+                  Profile section includes a form to add/update profile with
+                  personal details, social links and avatar.
+                </IconText>
+              </DashboardBottomBox>
 
-                <DashboardBottomBox>
-                  <Icon style={{ background: "#4ca1ff" }}>
-                    <img
-                      src={icon2}
-                      style={{ height: "40px", width: "40px" }}
-                      alt=""
-                    />
-                  </Icon>
-                  <IconText><span style={{'color': '#fff'}}>Create</span> an eye-catchy talk with all the information. You can submit a maximum of three talks.</IconText>
-                </DashboardBottomBox>
+              <DashboardBottomBox>
+                <Icon style={{ background: "#4ca1ff" }}>
+                  <img
+                    src={icon2}
+                    style={{ height: "40px", width: "40px" }}
+                    alt=""
+                  />
+                </Icon>
+                <IconText>
+                  <span style={{ color: "#fff" }}>Create</span> an eye-catchy
+                  talk with all the information. You can submit a maximum of
+                  three talks.
+                </IconText>
+              </DashboardBottomBox>
 
-                <DashboardBottomBox>
-                  <Icon>
-                    <img
-                      src={icon3}
-                      style={{ height: "40px", width: "40px" }}
-                      alt=""
-                    />
-                  </Icon>
-                  <IconText><span style={{'color': '#fff'}}>Sit back and relax!</span> You will see the update on the talk if it's selected or not on the dashboard itself. </IconText>
-                </DashboardBottomBox>
+              <DashboardBottomBox>
+                <Icon>
+                  <img
+                    src={icon3}
+                    style={{ height: "40px", width: "40px" }}
+                    alt=""
+                  />
+                </Icon>
+                <IconText>
+                  <span style={{ color: "#fff" }}>Sit back and relax!</span> You
+                  will see the update on the talk if it's selected or not on the
+                  dashboard itself.{" "}
+                </IconText>
+              </DashboardBottomBox>
 
-                <DashboardBottomBox>
-                    <DashboardCount color={"orange"}>2</DashboardCount>
-                    <DashboardText color={"orange"}>Talks Selected</DashboardText>
-                </DashboardBottomBox>
+              <DashboardBottomBox>
+                <DashboardCount color={"orange"}>2</DashboardCount>
+                <DashboardText color={"orange"}>Talks Selected</DashboardText>
+              </DashboardBottomBox>
 
-                <DashboardBottomBox>
-                    <DashboardCount color="weird">NO</DashboardCount>
-                    <DashboardText color={"weird"}>Profile Created</DashboardText>
-                </DashboardBottomBox>
-                <DashboardBottomBox>
-                    <DashboardCount color="orange">2</DashboardCount>
-                    <DashboardText color="orange">Talks Submitted</DashboardText>
-                </DashboardBottomBox>
-              </BottomFlex>
+              <DashboardBottomBox>
+                <DashboardCount color="weird">NO</DashboardCount>
+                <DashboardText color={"weird"}>Profile Created</DashboardText>
+              </DashboardBottomBox>
+              <DashboardBottomBox>
+                <DashboardCount color="orange">2</DashboardCount>
+                <DashboardText color="orange">Talks Submitted</DashboardText>
+              </DashboardBottomBox>
             </DashboardBottom>
           </DashboardContainer>
         </InnerContainer>
@@ -103,4 +115,11 @@ class Dashboard extends Component {
   }
 }
 
-export default Dashboard;
+const mapStateToProps = state => ({
+  dashboard: state.dashboard
+});
+
+export default connect(
+  mapStateToProps,
+  { getDashboard }
+)(withRouter(Dashboard));
