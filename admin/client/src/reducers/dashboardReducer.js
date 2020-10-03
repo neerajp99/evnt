2aed4db120046902603621e99f9a09564811b5ac
neerajp99 <neerajp1999@gmail.com>
2020-10-04 04:06:06 +0530

    Add dashboard reducer

---
+++ admin/client/src/reducers/dashboardReducer.js
@@ -0,0 +1,24 @@
+import {FETCH_TALKS, GET_ERRORS, LOADING_DASHBOARD} from "../actions/types"
+
+const initialState = {
+    allTalks: null,
+    loading: false
+}
+
+export default function(state = initialState, action) {
+    switch (action.type){
+        case FETCH_TALKS:
+            return {
+                ...state,
+                allTalks: action.payload,
+                loading: false
+            }
+        case LOADING_DASHBOARD:
+            return {
+                ...state,
+                loading: true 
+            }
+        default:
+            return state
+    }
+}
--- admin/client/src/reducers/index.js
+++ admin/client/src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import authReducer from "./authReducer";
 import profileReducer from "./profileRedcuer";
 import eventReducer from "./eventReducer";
+import dashboardReducer from "./dashboardReducer"
 
 export default combineReducers({
   auth: authReducer,
   profile: profileReducer,
-  event: eventReducer
+  event: eventReducer,
+  dashboard: dashboardReducer,
 });
