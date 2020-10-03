375b133b70bc978792a1d33ea581607f2f47ef4c
neerajp99 <neerajp1999@gmail.com>
2020-10-03 23:46:27 +0530

    Add talk model

---
+++ admin/models/Talk.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const talkSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "speakerAuth"
+  },
+  title: {
+    type: String,
+    isRequired: true
+  },
+  elevatorPitch: {
+    type: String,
+    isRequired: true
+  },
+  talkDuration: {
+    type: String,
+    isRequired: true
+  },
+  audienceLevel: {
+    type: String,
+    enum: ["All", "Intermediate", "Beginner", "Expert"]
+  },
+  description: {
+    type: String
+  },
+  additionalDetails: {
+    type: String
+  },
+  outcome: {
+    type: String
+  },
+  shortlisted: {
+    type: String,
+    default: "false"
+  },
+  talkTags: []
+});
+
+module.exports = Talk = mongoose.model("talks", talkSchema);
