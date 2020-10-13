0e9335f18568466aa0329e5d9200c3f5c6a52860
neerajp99 <neerajp1999@gmail.com>
2020-10-13 05:55:33 +0530

    Modified collaborator actions

--- admin/client/src/actions/collaboratorActions.js
+++ admin/client/src/actions/collaboratorActions.js
@@ -3,15 +3,18 @@ import { CREATE_COLLABORATOR, GET_ERRORS } from "./types"
 
 // Add collaborators 
 export const addCollaborators = (content) => dispatch => {
-    axios
-    .post("/api/collaborator/add", content)
-    .then(response => {
-        console.log('Response received: ', response)
-    })
-    .catch(error => {
-        dispatch({
-            type: GET_ERRORS,
-            payload: error.response.data 
+    // Loop over for N collaborators 
+    for (let i = 0 ; i < content.length ; i++) {
+        axios
+        .post("/api/collaborator/add", content[i])
+        .then(response => {
+            console.log('Response received: ', response)
         })
-    })
+        .catch(error => {
+            dispatch({
+                type: GET_ERRORS,
+                payload: error.response.data 
+            })
+        })
+    }
 }
