90977785023f79194749520ae06396703ffd122a
neerajp99 <neerajp1999@gmail.com>
2020-11-16 21:53:03 +0530

    Initial cfp reducer

---
+++ admin/client/src/reducers/cfpReducer.js
@@ -0,0 +1,20 @@
+import {GET_ERRORS, FETCH_CFP} from "../actions/types";
+
+const initialState = {
+    cfp: {},
+    loading: false 
+}
+
+export default function(state = initialState, action) {
+    switch(action.type){
+        case FETCH_CFP:
+            return {
+                ...state,
+                cfp: action.payload,
+                loading: false 
+            }
+
+        default:
+            return state
+    }
+}
