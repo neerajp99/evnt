c7bebcdd1ba0b48cf5978e53162edef67272a939
neerajp99 <neerajp1999@gmail.com>
2021-01-01 20:36:35 +0530

    Update cfp route

--- admin/routes/api/cfp.js
+++ admin/routes/api/cfp.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Cfp = require('../../models/Cfp');
 const passport = require('passport');
+const { update } = require('../../models/Cfp');
 
 // @route GET 
 // @description Fetch all cfp talks 
@@ -12,8 +13,27 @@ router.get('/', passport.authenticate("jwt", { session: false }), (req, res) => 
             res.status(200).json(cfp)
         })
         .catch(error => {
-            res.status(404).json()
+            res.status(404).json(error)
         })
+})
+
+// @route POST 
+// @description Update talks orientation 
+// @access PRIVATE 
+router.post('/updateCfp', passport.authenticate('jwt', { session: false }), (req, res) => {
+    Cfp.find()
+    .then(cfp => {
+        // const updatedTalk = {}
+        // updatedTalk.all = req.body.all
+        // updatedTalk.shortlisted = req.body.shortlisted 
+        // updatedTalk.final = req.body.final 
+        // console.log(cfp)
+        res.status(200).json(cfp)
+
+    })
+    .catch(error => {
+        res.status(404).json(error)
+    })
 })
 
 
